b567d4212cac694098f347109172bfcad6621e0b
AVKostin <alexander.kostin.ua@gmail.com>
2022-05-17 22:40:05 +0300

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,8 +1,8 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Wrapper, Form, Input, Button } from './Searchbar.styled';
-import { Notify } from 'notiflix';
 import { BsSearch } from 'react-icons/bs';
+import { Notify } from 'notiflix';
 
 Notify.init({
   position: 'center-top',
@@ -10,46 +10,42 @@ Notify.init({
   fontSize: '18px',
 });
 
-export default class Searchbar extends Component {
-  state = {
-    query: '',
+export default function Searchbar({ onSubmit }) {
+  const [query, setQuery] = useState('');
+
+  const handleQueryChange = event => {
+    setQuery(event.currentTarget.value.toLowerCase());
   };
 
-  handleQueryChange = event => {
-    this.setState({ query: event.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
 
-    if (this.state.query.trim() === '') {
+    if (query.trim() === '') {
       Notify.failure('please enter any query');
       return;
     }
 
-    this.props.onSubmit(this.state.query.trim());
-    this.setState({ query: '' });
+    onSubmit(query.trim());
+    setQuery('');
   };
 
-  render() {
-    return (
-      <Wrapper>
-        <Form onSubmit={this.handleSubmit} id="search-form">
-          <Input
-            type="text"
-            name="searchQuery"
-            autoComplete="off"
-            placeholder="Free Search Images By Name..."
-            value={this.state.query}
-            onChange={this.handleQueryChange}
-          />
-          <Button type="submit">
-            <BsSearch size={27} />
-          </Button>
-        </Form>
-      </Wrapper>
-    );
-  }
+  return (
+    <Wrapper>
+      <Form onSubmit={handleSubmit} id="search-form">
+        <Input
+          type="text"
+          name="searchQuery"
+          autoComplete="off"
+          placeholder="Free Search Images By Name..."
+          value={query}
+          onChange={handleQueryChange}
+        />
+        <Button type="submit">
+          <BsSearch size={27} />
+        </Button>
+      </Form>
+    </Wrapper>
+  );
 }
 
 Searchbar.propTypes = {
