1a29bad60f88920374e9e66ca4ad8ecf9f92f864
AVKostin <alexander.kostin.ua@gmail.com>
2022-05-17 14:54:24 +0300

    Create ImageGalleryItem.jsx

---
+++ src/components/ImageGallery/ImageGalleryItem.jsx
@@ -0,0 +1,84 @@
+import PropTypes from 'prop-types';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import {
+  ImageItem,
+  ImageWrapper,
+  Image,
+  InfoWrapper,
+  Info,
+  Comments,
+} from './ImageGalleryItem.styled';
+import {
+  TiMessages,
+  TiThumbsUp,
+  TiEyeOutline,
+  TiDownload,
+} from 'react-icons/ti';
+
+const ImageGalleryItem = ({
+  setInfoForModal,
+  id,
+  webformatURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+  largeImageURL,
+}) => {
+  const handleClick = () => {
+    Loading.arrows({ svgSize: '120px' });
+    setInfoForModal(largeImageURL, tags);
+    Loading.remove();
+  };
+  return (
+    <ImageItem key={id} onClick={handleClick}>
+      <div>
+        <ImageWrapper>
+          <Image src={webformatURL} alt={tags} loading="lazy" />
+        </ImageWrapper>
+
+        <InfoWrapper>
+          <Info>
+            <b>
+              <TiEyeOutline size={26} color="#000000" />
+            </b>
+            <Comments>{views}</Comments>
+          </Info>
+          <Info>
+            <b>
+              <TiThumbsUp size={26} color="#000000" />
+            </b>
+            <Comments> {likes}</Comments>
+          </Info>
+          <Info>
+            <b>
+              <TiDownload size={26} color="#000000" />
+            </b>
+            <Comments>{downloads}</Comments>
+          </Info>
+          <Info>
+            <b>
+              <TiMessages size={26} color="#000000" />
+            </b>
+            <Comments>{comments}</Comments>
+          </Info>
+        </InfoWrapper>
+      </div>
+    </ImageItem>
+  );
+};
+
+ImageGalleryItem.propTypes = {
+  largeImageURL: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  webformatURL: PropTypes.string.isRequired,
+  tags: PropTypes.string.isRequired,
+  setInfoForModal: PropTypes.func.isRequired,
+  likes: PropTypes.number.isRequired,
+  views: PropTypes.number.isRequired,
+  comments: PropTypes.number.isRequired,
+  downloads: PropTypes.number.isRequired,
+};
+
+export default ImageGalleryItem;
