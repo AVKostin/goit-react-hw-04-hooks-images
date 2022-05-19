f0c05574cf604fbcc77fa96871257c615742741a
AVKostin <alexander.kostin.ua@gmail.com>
2022-05-19 19:27:39 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,11 @@
-import { Component } from 'react';
-import Searchbar from '../Searchbar';
 import SearchInfo from '../SearchInfo/SearchInfo';
 import ScrollOnTop from '../ScrollToTop';
-class App extends Component {
-  state = {
-    query: '',
-  };
-  handleFormSubmit = query => {
-    this.setState({ query });
-  };
 
-  render() {
-    const { query } = this.state;
-    return (
-      <>
-        <header>
-          <Searchbar onSubmit={this.handleFormSubmit} />
-        </header>
-        <main>
-          <SearchInfo query={query} />
-          <ScrollOnTop />
-        </main>
-      </>
-    );
-  }
+export default function App() {
+  return (
+    <>
+      <SearchInfo />
+      <ScrollOnTop />
+    </>
+  );
 }
-export default App;
