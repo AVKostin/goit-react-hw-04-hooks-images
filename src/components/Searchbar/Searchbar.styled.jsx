a858d8326398b6c98c9b8163903fdad02cf2cc2b
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-10-16 00:15:08 +0300

    refactoring

--- src/components/Searchbar/Searchbar.styled.jsx
+++ src/components/Searchbar/Searchbar.styled.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Wrapper = styled.div`
   background-color: rgba(250, 225, 173, 0.55);
@@ -10,30 +10,33 @@ export const Wrapper = styled.div`
   left: 0;
   z-index: 100;
 `;
+
 export const Form = styled.form`
-  border: 3px solid rgb(177 116 22);
+  margin: 0 auto;
   width: 380px;
+  padding: 5px;
+  border: 3px solid rgb(177 116 22);
   display: flex;
   flex-direction: row-reverse;
-  padding: 5px;
-  z-index: 6;
   justify-content: center;
   background-color: rgb(235, 252, 255);
-  margin: 0 auto;
-  opacity: 1;
   border-radius: 5px;
   box-shadow: 2px 3px 1px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.08),
-    0px 2px 2px rgba(0, 0, 0, 0.12);
+  0px 2px 2px rgba(0, 0, 0, 0.12);
+  opacity: 1;
+  z-index: 6;
 `;
+
 export const Input = styled.input`
   font-size: 26px;
   font-weight: 700;
 
   background-color: transparent;
-  &::placeholder {
-    font-size: 20px;
+    &::placeholder {
+      font-size: 20px;
   }
 `;
+
 export const Button = styled.button`
   margin-top: 5px;
   background-color: transparent;
@@ -42,8 +45,8 @@ export const Button = styled.button`
   color: gray;
   transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1),
     color 250ms cubic-bezier(0.4, 0, 0.2, 1);
-  &:hover,
-  &:focus {
-    fill: rgb(7, 184, 16);
+      &:hover,
+      &:focus {
+        fill: rgb(7, 184, 16);
   }
 `;
