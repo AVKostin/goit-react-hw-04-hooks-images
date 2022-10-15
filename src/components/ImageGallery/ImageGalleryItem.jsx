adca256c1eee00dd3136d3d3e1bd9e0b0b42af96
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-10-16 00:15:43 +0300

    refactoring

--- src/components/ImageGallery/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem.jsx
@@ -1,86 +1,82 @@
-import PropTypes from 'prop-types';
-import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import PropTypes from "prop-types";
+import { Loading } from "notiflix/build/notiflix-loading-aio";
 import {
-  ImageItem,
-  ImageWrapper,
-  Image,
-  InfoWrapper,
-  Info,
-  Comments,
-} from './ImageGalleryItem.styled';
+    Info,
+    Image,
+    Comments,
+    ImageItem,
+    InfoWrapper,
+    ImageWrapper,
+} from "./ImageGalleryItem.styled";
 
 import {
-  TiMessages,
-  TiThumbsUp,
-  TiEyeOutline,
-  TiDownload,
-} from 'react-icons/ti';
+    TiMessages,
+    TiThumbsUp,
+    TiDownload,
+    TiEyeOutline,
+} from "react-icons/ti";
 
 export default function ImageGalleryItem({
-  setInfoForModal,
-  id,
-  webformatURL,
-  tags,
-  likes,
-  views,
-  comments,
-  downloads,
-  largeImageURL,
+    setInfoForModal,
+    id,
+    webformatURL,
+    largeImageURL,
+    tags,
+    likes,
+    views,
+    comments,
+    downloads,
 }) {
-  const handleClick = () => {
-    Loading.arrows({ svgSize: '120px' });
-    setInfoForModal(largeImageURL, tags);
-    Loading.remove();
-  };
-  return (
-    <ImageItem key={id} onClick={handleClick}>
-      <div>
-        <ImageWrapper>
-          <Image src={webformatURL} alt={tags} loading="lazy" />
-        </ImageWrapper>
-
-        <InfoWrapper>
-          <Info>
-            <b>
-              <TiEyeOutline size={26} color="#000000" />
-            </b>
-            <Comments>{views}</Comments>
-          </Info>
-
-          <Info>
-            <b>
-              <TiThumbsUp size={26} color="#000000" />
-            </b>
-            <Comments> {likes}</Comments>
-          </Info>
-
-          <Info>
-            <b>
-              <TiDownload size={26} color="#000000" />
-            </b>
-            <Comments>{downloads}</Comments>
-          </Info>
-
-          <Info>
-            <b>
-              <TiMessages size={26} color="#000000" />
-            </b>
-            <Comments>{comments}</Comments>
-          </Info>
-        </InfoWrapper>
-      </div>
-    </ImageItem>
-  );
+    const handleClick = () => {
+        Loading.arrows({ svgSize: "120px" });
+        setInfoForModal(largeImageURL, tags);
+        Loading.remove();
+    };
+    return (
+        <ImageItem key={id} onClick={handleClick}>
+            <div>
+                <ImageWrapper>
+                    <Image src={webformatURL} alt={tags} loading="lazy" />
+                </ImageWrapper>
+                <InfoWrapper>
+                    <Info>
+                        <b>
+                            <TiEyeOutline size={26} color="#000000" />
+                        </b>
+                        <Comments>{views}</Comments>
+                    </Info>
+                    <Info>
+                        <b>
+                            <TiThumbsUp size={26} color="#000000" />
+                        </b>
+                        <Comments> {likes}</Comments>
+                    </Info>
+                    <Info>
+                        <b>
+                            <TiDownload size={26} color="#000000" />
+                        </b>
+                        <Comments>{downloads}</Comments>
+                    </Info>
+                    <Info>
+                        <b>
+                            <TiMessages size={26} color="#000000" />
+                        </b>
+                        <Comments>{comments}</Comments>
+                    </Info>
+                </InfoWrapper>
+            </div>
+        </ImageItem>
+    );
 }
 
 ImageGalleryItem.propTypes = {
-  largeImageURL: PropTypes.string.isRequired,
-  id: PropTypes.number.isRequired,
-  webformatURL: PropTypes.string.isRequired,
-  tags: PropTypes.string.isRequired,
-  setInfoForModal: PropTypes.func.isRequired,
-  likes: PropTypes.number.isRequired,
-  views: PropTypes.number.isRequired,
-  comments: PropTypes.number.isRequired,
-  downloads: PropTypes.number.isRequired,
+    setInfoForModal: PropTypes.func.isRequired,
+    id: PropTypes.number.isRequired,
+    webformatURL: PropTypes.string.isRequired,
+    largeImageURL: PropTypes.string.isRequired,
+    tags: PropTypes.string.isRequired,
+    likes: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    comments: PropTypes.number.isRequired,
+    downloads: PropTypes.number.isRequired,
 };
