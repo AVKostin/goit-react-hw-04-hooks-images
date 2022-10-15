11c7e930186eaf14c5bc7d17f43bd7ed78397024
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-10-16 00:15:48 +0300

    refactoring

--- src/components/ImageGallery/ImageGallery.styled.jsx
+++ src/components/ImageGallery/ImageGallery.styled.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const ImageList = styled.ul`
   margin: 0 auto;
@@ -6,19 +6,16 @@ export const ImageList = styled.ul`
   gap: 25px;
   padding: 0 30px;
   margin-top: 192px;
-
-  @media screen and (min-width: 480px) {
-    width: 480px;
-    grid-template-columns: 1fr;
+    @media screen and (min-width: 480px) {
+        width: 480px;
+        grid-template-columns: 1fr;
   }
-
-  @media screen and (min-width: 768px) {
-    width: 768px;
-    grid-template-columns: 1fr 1fr;
+    @media screen and (min-width: 768px) {
+        width: 768px;
+        grid-template-columns: 1fr 1fr;
   }
-
-  @media screen and (min-width: 1280px) {
-    width: 1200px;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
+    @media screen and (min-width: 1280px) {
+        width: 1200px;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
   }
 `;
