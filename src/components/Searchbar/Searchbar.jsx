8ed314facd683184a27c2b31ccb093666e714207
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-10-16 00:15:22 +0300

    refactoring

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,55 +1,53 @@
-import { useState } from 'react';
-import PropTypes from 'prop-types';
-import { Wrapper, Form, Input, Button } from './Searchbar.styled';
-import { BsSearch } from 'react-icons/bs';
-import { Notify } from 'notiflix';
+import PropTypes from "prop-types";
+import { useState } from "react";
+import { Wrapper, Form, Input, Button } from "./Searchbar.styled";
+import { BsSearch } from "react-icons/bs";
+import { Notify } from "notiflix";
 
 Notify.init({
-  position: 'center-top',
-  width: '400px',
-  fontSize: '18px',
+    position: "center-top",
+    width: "400px",
+    fontSize: "18px",
 });
 
 export default function Searchbar({ onSubmit }) {
-  const [query, setQuery] = useState('');
+    const [query, setQuery] = useState("");
 
-  const handleQueryChange = event => {
-    setQuery(event.currentTarget.value.toLowerCase());
-  };
+    const handleQueryChange = (event) => {
+        setQuery(event.currentTarget.value.toLowerCase());
+    };
 
-  const handleSubmit = event => {
-    event.preventDefault();
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (query.trim() === "") {
+            Notify.failure("please enter any query");
+            return;
+        }
+        onSubmit(query.trim());
+        setQuery("");
+    };
 
-    if (query.trim() === '') {
-      Notify.failure('please enter any query');
-      return;
-    }
-
-    onSubmit(query.trim());
-    setQuery('');
-  };
-
-  return (
-    <header>
-      <Wrapper>
-        <Form onSubmit={handleSubmit} id="search-form">
-          <Input
-            type="text"
-            name="searchQuery"
-            autoComplete="off"
-            placeholder="Free Search Images By Name..."
-            value={query}
-            onChange={handleQueryChange}
-          />
-          <Button type="submit">
-            <BsSearch size={30} />
-          </Button>
-        </Form>
-      </Wrapper>
-    </header>
-  );
+    return (
+        <header>
+            <Wrapper>
+                <Form onSubmit={handleSubmit} id="search-form">
+                    <Input
+                        type="text"
+                        name="searchQuery"
+                        autoComplete="off"
+                        placeholder="Free Search Images By Name..."
+                        value={query}
+                        onChange={handleQueryChange}
+                    />
+                    <Button type="submit">
+                        <BsSearch size={30} />
+                    </Button>
+                </Form>
+            </Wrapper>
+        </header>
+    );
 }
 
 Searchbar.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
+    onSubmit: PropTypes.func.isRequired,
 };
