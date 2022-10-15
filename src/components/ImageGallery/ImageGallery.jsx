11f4cf095b9bfcd2f202401923cbe123cce0bc4d
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-10-16 00:15:57 +0300

    refactoring

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,51 +1,51 @@
-import PropTypes from 'prop-types';
-import ImageGalleryItem from './ImageGalleryItem';
-import { ImageList } from './ImageGallery.styled';
+import PropTypes from "prop-types";
+import ImageGalleryItem from "./ImageGalleryItem";
+import { ImageList } from "./ImageGallery.styled";
 
 export default function ImageGallery({ pictures, setInfoForModal }) {
-  return (
-    <ImageList>
-      {pictures.map(
-        ({
-          id,
-          webformatURL,
-          tags,
-          likes,
-          views,
-          comments,
-          downloads,
-          largeImageURL,
-        }) => (
-          <ImageGalleryItem
-            setInfoForModal={setInfoForModal}
-            key={id}
-            id={id}
-            webformatURL={webformatURL}
-            largeImageURL={largeImageURL}
-            tags={tags}
-            likes={likes}
-            views={views}
-            comments={comments}
-            downloads={downloads}
-          />
-        )
-      )}
-    </ImageList>
-  );
+    return (
+        <ImageList>
+            {pictures.map(
+                ({
+                    id,
+                    webformatURL,
+                    largeImageURL,
+                    tags,
+                    likes,
+                    views,
+                    comments,
+                    downloads,
+                }) => (
+                    <ImageGalleryItem
+                        setInfoForModal={setInfoForModal}
+                        key={id}
+                        id={id}
+                        webformatURL={webformatURL}
+                        largeImageURL={largeImageURL}
+                        tags={tags}
+                        likes={likes}
+                        views={views}
+                        comments={comments}
+                        downloads={downloads}
+                    />
+                ),
+            )}
+        </ImageList>
+    );
 }
 
 ImageGallery.propTypes = {
-  pictures: PropTypes.arrayOf(
-    PropTypes.shape({
-      largeImageURL: PropTypes.string.isRequired,
-      id: PropTypes.number.isRequired,
-      webformatURL: PropTypes.string.isRequired,
-      tags: PropTypes.string.isRequired,
-      likes: PropTypes.number.isRequired,
-      views: PropTypes.number.isRequired,
-      comments: PropTypes.number.isRequired,
-      downloads: PropTypes.number.isRequired,
-    })
-  ).isRequired,
-  setInfoForModal: PropTypes.func.isRequired,
+    pictures: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            webformatURL: PropTypes.string.isRequired,
+            largeImageURL: PropTypes.string.isRequired,
+            tags: PropTypes.string.isRequired,
+            likes: PropTypes.number.isRequired,
+            views: PropTypes.number.isRequired,
+            comments: PropTypes.number.isRequired,
+            downloads: PropTypes.number.isRequired,
+        }),
+    ).isRequired,
+    setInfoForModal: PropTypes.func.isRequired,
 };
